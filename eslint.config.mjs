c8c4f2a776207f3cbb360add64cf071f80757788
iqblfrdsyh <iqblfrdsyh@gmail.com>
2025-01-16 18:18:26 +0700

    feat(setup): setup husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-const { dirname } = require("path");
-const { fileURLToPath } = require("url");
-const { FlatCompat } = require("@eslint/eslintrc");
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -37,13 +37,13 @@ const TestimonialSlider = () => {
             <div className="flex justify-between max-w-5xl gap-[1.5rem] h-screen shrink-0 mx-auto overflow-hidden">
                 {[...new Array(cols)].map((_, i) => {
                     const colsDurations = [
-                        ['15000ms', '15000ms'], 
-                        ['23000ms', '23000ms'], 
-                        ['14000ms', '14000ms'], 
+                        ['15000ms', '15000ms'],
+                        ['23000ms', '23000ms'],
+                        ['14000ms', '14000ms'],
                     ];
 
                     return (
-                        <div className="flex flex-col overflow-hidden" key={i}>
+                        <div className="flex flex-col overflow-hidden" key={i + 1}>
                             {[...new Array(2)].map((_, j) => (
                                 <div
                                     className="max-w-none flex flex-col animate-infinite-scroll"
@@ -51,10 +51,11 @@ const TestimonialSlider = () => {
                                         animationDuration: colsDurations[i][j],
                                     }}
                                     aria-hidden={i % 2 === 0 ? true : false}
+                                    key={j + 1}
                                 >
                                     {testimonials
                                         .map((testimonial, k) => (
-                                            <div className="p-6" key={k}>
+                                            <div className="p-6" key={k + 1}>
                                                 <Cards.CardTestimoni
                                                     name={testimonial.name}
                                                     src={testimonial.image}
