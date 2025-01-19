6d6f1904ff00edb9daa6484b710c97cb08716b42
iqblfrdsyh <iqblfrdsyh@gmail.com>
2025-01-19 17:54:59 +0700

    refactor: move all types to a new 'types' folder

---
+++ src/types/types.ts
@@ -0,0 +1,51 @@
+export interface IMenuItems
+  extends Array<{
+    label: string;
+    href: string;
+    section: string;
+  }> {}
+
+export interface IStatistic {
+  id: number;
+  label: string;
+  value: number | string;
+}
+export interface IBenefit {
+  id: number;
+  image: string;
+  label: string;
+  description: string;
+}
+
+export interface IService {
+  id: number;
+  image: string;
+  label: string;
+  link: string;
+}
+
+export interface ITestimonial {
+  id: number;
+  text: string;
+  name: string;
+  image: string;
+}
+export interface ICount {
+  data?: {
+    id: number;
+    label: string;
+    value: number | string;
+  }[];
+}
+
+export interface ICardTestimoni {
+  src: string;
+  name: string;
+  text: string;
+}
+
+export interface ICardService {
+  src: string;
+  title: string;
+  link: string;
+}
